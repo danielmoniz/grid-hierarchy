9a3f18c04a77689eeb5655e7e55fb21f8338c428
Daniel Moniz <daniel.moniz@gmail.com>
2018-08-03 12:34:22 -0400

    Allow Grid to find tiles/columns more easily.

--- grid/grid.js
+++ grid/grid.js
@@ -2,9 +2,8 @@
 // @TODO Use LinkedList instead of array. Splicing items into an array is (surely?) be less efficient than inserting into a LinkedList, especially if iteration is already performed.
 
 module.exports.Grid = (function() {
-  function Grid(gridSize, zeroes) {
+  function Grid(gridSize) {
     this.gridSize = gridSize || 1;
-    this.zeroesInKey = zeroes || 0;
     this.columns = [];
   }
 
@@ -26,6 +25,50 @@ module.exports.Grid = (function() {
       }
     }
     return []
+  }
+
+  // @TODO Have a method that iterates over a column (from one x value to another) and returns the data in all of those tiles
+  Grid.prototype.getFromColumn = function(trueY, trueMinX, trueMaxX) {
+    var y = getGridValue(trueY, this.gridSize);
+    var minX = getGridValue(trueMinX, this.gridSize);
+    var maxX = getGridValue(trueMaxX, this.gridSize);
+
+    var column = this.findColumn(y);
+  }
+
+  Grid.prototype.findColumn = function(y) {
+    for (var i = 0; i < this.columns.length; i++) {
+      var column = this.columns[i];
+      if (column.y > y) { break }
+      if (column.y < y) { continue }
+      return column
+    }
+    return getNewColumn(y)
+  }
+
+  /*
+   * Returns a list of all tiles in a given range within a column. Needs to be given a column number, not an actual x value.
+   */
+  Grid.prototype.getTilesInRange = function(column, minY, maxY) {
+    var allTiles = [];
+    for (var i = 0; i < column.tiles.length; i++) {
+      var tile = column.tiles[i];
+      if (tile.y < minY) { continue }
+      if (tile.y > maxY) { break }
+      allTiles.push(tile);
+    }
+    return allTiles;
+  }
+
+  /*
+   * Returns the combined data from a set of tiles.
+   */
+  Grid.prototype.getEntitiesFromTiles = function(tiles) {
+    var allEntities = [];
+    tiles.forEach(function(tile) {
+      allEntities = allEntities.concat(tile.data)
+    });
+    return allEntities;
   }
 
   /*
--- tests/grid.unit.test.js
+++ tests/grid.unit.test.js
@@ -1,10 +1,8 @@
 
 var Grid = require('grid').Grid;
 
-console.log(Grid);
-
 describe('Grid', function() {
-  describe('add', function() {
+  describe('add/get', function() {
     it('should add an entity to its location in the grid', function() {
       var grid = new Grid();
       var entity = { x: 3, y: 4 };
@@ -55,6 +53,75 @@ describe('Grid', function() {
       expect(grid.get(1, 1)[0]).toBe(entity);
     })
 
+    it('should only return entities if in same region', function() {
+      var grid = new Grid(6);
+      grid.add({ x: 5, y: 3, name: 'One' });
+      grid.add({ x: 4, y: 4, name: 'Two' });
+      grid.add({ x: 7, y: 8, name: 'Other' });
+
+      expect(grid.get(0, 0).length).toBe(2);
+      expect(grid.get(0, 0)[0].name).toBe('One');
+      expect(grid.get(0, 0)[1].name).toBe('Two');
+    })
+
+  })
+
+  describe('findColumn', function() {
+    it('should return an empty column if no column can be found', function() {
+      var grid = new Grid(6);
+
+      var column = grid.findColumn(19);
+      expect(column.tiles.length).toBe(0);
+    })
+
+    it('should return a column with the relevant tiles', function() {
+      var grid = new Grid(4);
+      grid.add({ x: 5, y: 2, name: 'One' }); // tile 1
+      grid.add({ x: 5, y: 6, name: 'Two' }); // tile 2
+      grid.add({ x: 5, y: 6, name: 'Three' }); // tile 2
+      grid.add({ x: 9, y: 6, name: 'Other' }); // different column
+
+      var column = grid.findColumn(1);
+      expect(column.tiles.length).toBe(2);
+      expect(column.tiles[0].data[0].name).toBe('One');
+      expect(column.tiles[1].data[0].name).toBe('Two');
+    })
+  })
+
+  describe('getTilesInRange', function() {
+    it('should return a list of all tiles in a range within a column', function() {
+      var grid = new Grid(4);
+      grid.add({ x: 5, y: 6, name: 'One' }); // tile 1, y=1
+      grid.add({ x: 5, y: 9, name: 'Two' }); // tile 2, y=2
+      grid.add({ x: 5, y: 9, name: 'Three' }); // tile 2, y=2
+      grid.add({ x: 5, y: 15, name: 'Other' }); // outside tile, tile 3
+      grid.add({ x: 9, y: 6, name: 'Other' }); // different column
+
+      var column = grid.columns[0];
+      var tiles = grid.getTilesInRange(column, 0, 2);
+      expect(tiles.length).toBe(2);
+      expect(tiles[0].y).toBe(1);
+      expect(tiles[1].y).toBe(2);
+    })
+  })
+
+  describe('getEntitiesFromTiles', function() {
+    it('should return a combined list of all entities within a set of tiles', function() {
+      var grid = new Grid(4);
+      grid.add({ x: 5, y: 2, name: 'One' }); // tile 1
+      grid.add({ x: 5, y: 6, name: 'Two' }); // tile 2
+      grid.add({ x: 5, y: 6, name: 'Three' }); // tile 2
+      grid.add({ x: 9, y: 6, name: 'Other' }); // different column
+
+      var tiles = grid.columns[0].tiles;
+      expect(tiles.length).toBe(2);
+
+      var entities = grid.getEntitiesFromTiles(tiles);
+      expect(entities.length).toBe(3);
+      expect(entities[0].name).toBe('One');
+      expect(entities[1].name).toBe('Two');
+      expect(entities[2].name).toBe('Three');
+    })
   })
 
 })
